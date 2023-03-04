a6f1b18dbbe7e8b490c6d6fbdb23ccfaa4c7f6b9
vamsi <vamsikrishna.chittam@gmail.com>
2023-03-04 07:56:17 +0530

    user script

--- user.sh
+++ user.sh
@@ -11,3 +11,10 @@ mv user-main user
 cd /home/roboshop/user
 npm install
 
+
+
+
+# mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+# systemctl daemon-reload
+# systemctl start user
+# systemctl enable user
