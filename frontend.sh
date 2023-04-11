b3bda8dd99ae3d471c199aa1a0e899a5e4308d0c
vamsi <vamsikrishna.chittam@gmail.com>
2023-04-11 17:16:23 +0530

    Roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,29 @@
-yum install nginx -y
+COMPONENT=frontend
+CONTENT="*"
+source common.sh
 
+PRINT "Install Nginx"
+yum install nginx -y &>>$LOG
+STAT $?
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+APP_LOC=/usr/share/nginx/html
 
-cd /usr/share/nginx/html
-rm -rf*
-unzip /tmp/frontend.zip
+DOWNLOAD_APP_CODE
+
 mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx
-systemctl restart nginx
+PRINT "Copy RoboShop Configuration File"
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
+
+PRINT "Update RoboShop Configuration"
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.devopsb69.online/'  -e '/user/ s/localhost/dev-user.devopsb69.online/' -e '/cart/ s/localhost/dev-cart.devopsb69.online/' -e '/shipping/ s/localhost/dev-shipping.devopsb69.online/' -e '/payment/ s/localhost/dev-payment.devopsb69.online/' /etc/nginx/default.d/roboshop.conf
+STAT $?
+
+PRINT "Enable Nginx Service"
+systemctl enable nginx &>>$LOG
+STAT $?
+
+PRINT "Start Nginx Service"
+systemctl restart nginx &>>$LOG
+STAT $?
