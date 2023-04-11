080309680cce2aa5ffe1bfb0254663d354bad7f2
vamsi <vamsikrishna.chittam@gmail.com>
2023-04-11 17:21:23 +0530

    Roboshop-shell

--- user.sh
+++ user.sh
@@ -1,21 +1,4 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+COMPONENT=user
+source common.sh
 
-useradd roboshop
-
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
-cd /home/roboshop
-rm -rf user
-unzip -o /tmp/user.zip
-mv user-main user
-cd /home/roboshop/user
-npm install
-
-
-sed -i -e 's/REDIS_ENDPOINT/redis.vamsy.online/' -e 's/MONGO.ENDPOINT/mongo.vamsy.online/' /home/roboshop/user/ systemd.service
-
-
-mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
-systemctl daemon-reload
-systemctl start user
-systemctl enable user
+NODEJS
