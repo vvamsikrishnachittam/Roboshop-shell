c4b86e67e7099f749fa73f429d1770da4ea055af
vamsi <vamsikrishna.chittam@gmail.com>
2023-04-11 17:19:53 +0530

    Roboshop-shell

--- catalouge.sh
+++ catalouge.sh
@@ -1,19 +1,4 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+COMPONENT=catalogue
+source common.sh
 
-useradd roboshop
-
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-cd /home/roboshop
-rm -rf catalogue
-unzip -o /tmp/catalogue.zip
-mv catalogue-main catalogue
-cd /home/roboshop/catalogue
-npm install
-
-sed -i -e 's/MONGOD_DNSNAME/mongod.vamsy.online/' systemd.service
-
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl restart catalogue
-systemctl enable catalogue
+NODEJS
