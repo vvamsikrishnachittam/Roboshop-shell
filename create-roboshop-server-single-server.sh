6d3708773348754d6847e0c96abab1a1aab91718
vamsi <vamsikrishna.chittam@gmail.com>
2023-04-12 16:04:09 +0530

    Roboshop-shell

--- create-roboshop-server-single-server.sh
+++ create-roboshop-server-single-server.sh
@@ -16,7 +16,8 @@ create_ec2() {
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-exit
+
+
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
